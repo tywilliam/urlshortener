a7c67776399caa47607e4cd975b739a20d84e2ec
Tyrese Williams <tyresewilliams@Tyreses-MacBook-Pro.local>
2019-07-09 17:41:21 -0400

    README

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## URL Shortener
+
+Code can be used for a project. 
+
+I've seen multiple ways of solving this problem this was one done by receiving input of a URL and storing it inside a database a long with the short url code and retrieving the code to bring back the input URL. 
--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ const Url = require('../models/Url');
 router.get('/:code', async (req, res) => {
     try {
         const url = await Url.findOne({ urlCode: req.params.code })
-        if(url) {
-            return res.redirect(url..longUrl)
-        } else {
-            return res.status(404).json({ 'No url found' });
-        }
+            if(url) {
+                return res.redirect(url..longUrl)
+            } else {
+                return res.status(404).json({ 'No url found' });
+            }
     } catch(err) {
         console.error(err);
         res.status(500).json('Server error');
